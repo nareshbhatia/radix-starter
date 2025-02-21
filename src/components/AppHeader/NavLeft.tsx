63f7b2e23cedce1fa3628c9e740f03258802aa47
Naresh Bhatia <me@nareshbhatia.dev>
2025-02-21 12:34:05 -0500

    update docs and dependencies

--- src/components/AppHeader/NavLeft.tsx
+++ src/components/AppHeader/NavLeft.tsx
@@ -4,7 +4,7 @@ import { Text } from '@radix-ui/themes';
 export function NavLeft() {
   return (
     <nav className={styles.nav}>
-      <Text weight="bold">Auth Console</Text>
+      <Text weight="bold">Radix Starter</Text>
     </nav>
   );
 }
